2576cba6e100f5e3884649cb591f2fa16cf8e734
pranit <pranit@intellero.com>
2020-10-21 12:18:37 +0530

    updatd jistsi

--- app/src/main/java/com/example/mycustomlibrary/MainActivity.java
+++ app/src/main/java/com/example/mycustomlibrary/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.jitsisdk.PranitClass;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PranitClass.print(this,"WELCOME TO CUSTOM LIBRARY");
     }
 }
